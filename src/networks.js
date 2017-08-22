f1fda00a64fac38af938ccc61e3cb2b7f9ffc60d
Jacob Bryan <jake@jakebryan.me>
2017-08-22 17:48:43 +0200

    Added tDash and Dash to the list of networks so I can use bitcoinjs/bip32-utils

--- src/networks.js
+++ src/networks.js
@@ -31,5 +31,25 @@ module.exports = {
     pubKeyHash: 0x30,
     scriptHash: 0x32,
     wif: 0xb0
+  },
+  dash: {
+    messagePrefix: '\x1dDash Signed Message:\n',
+    bip32: {
+      public: 0x0488b21e,
+      private: 0x0488ade4
+    },
+    pubKeyHash: 0x4c,
+    scriptHash: 0x0f,
+    wif: 0xcc
+  },
+  tDash: {
+    messagePrefix: '\x1dDash Signed Message:\n',
+    bip32: {
+      public: 0x043587cf,
+      private: 0x04358394
+    },
+    pubKeyHash: 0x8c,
+    scriptHash: 0x13,
+    wif: 0xef
   }
 }
